4f7bd75e0fd40c359bcc0e936b2ef69f1273aa8c
congtrieu <trieudc@devblock.net>
2020-12-17 09:04:47 +0700

    update dependency

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,8 +1,10 @@
+import { injectable } from 'inversify';
+import 'reflect-metadata';
 export interface Util {
   formatString(value: string, variables: object): string;
 }
 
-
+@injectable()
 export class UtilImpl {
   public formatString(value: string, variables: object) {
     if (!value) {
