f24b571e14ae42d9d9dd263a767ebba03b85a8f8
trieudc <trieudc@devblock.net>
2020-12-17 10:32:15 +0700

    update sutructure class

--- dist/helpers/utils.d.ts
+++ dist/helpers/utils.d.ts
@@ -3,6 +3,6 @@ import 'reflect-metadata';
 export interface Util {
     formatString(value: string, variables: object): string;
 }
-export declare class UtilImpl {
+export declare class UtilImpl implements Util {
     formatString(value: string, variables: object): string;
 }
